61aa9d1714bf8cb264ddf5154a75920072dedb68
Kezia <roselinakeziarijadi@gmail.com>
2026-03-02 22:21:14 -0800

    init

---
+++ src/common.h
@@ -0,0 +1,10 @@
+// refresher that this first checks that preprocessor macro is undefined
+#ifndef clox_common_h
+#define clox_common_h
+
+#include <stdbool.h>
+#include <stdbool.h>
+#include <stdbool.h>
+#include <stdbool.h>
+
+#endif
