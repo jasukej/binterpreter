e2578337a6369f200a7657a80c8b33a3fdcbf993
Kezia <roselinakeziarijadi@gmail.com>
2026-03-21 17:22:01 -0700

    chore: codegen utils

--- src/compiler.c
+++ src/compiler.c
@@ -13,6 +13,12 @@ typedef struct {
 } Parser;
 
 Parser parser;
+Chunk* compilingChunk;
+
+// TODO: Logic on chunk pointer to be returned for user-defined functions
+static Chunk* currentChunk() {
+    return compilingChunk;
+}
 
 static void errorAt(Token* token, const char* message) {
     if (parser.panicMode) return;
@@ -56,15 +62,41 @@ static void consume(TokenType type, const char* message) {
     errorAtCurrent(message);
 }
 
+// Write a byte to our current chunk
+static void emitByte(uint8_t byte) {
+    writeChunk(currentChunk(), byte, parser.previous.line);
+}
+
+// Add the return to the end of a chunk to print the output
+static void emitReturn() {
+    emitByte(OP_RETURN);
+}
+
+// Write two bytes, typically forb writing opcode, then operand
+static void emitBytes(uint8_t byte1, uint8_t byte2) {
+    emitByte(byte1);
+    emitByte(byte2);
+}
+
+static void endCompiler() {
+    emitReturn();
+}
+
+static void expression() {
+    // TODO
+}
+
 // Performs single pass compilation to output bytecode
 bool compile(const char* source, Chunk* chunk) {
     initScanner(source);
-    
+    compilingChunk = chunk;
+
     parser.hadError = false;
     parser.panicMode = false;
 
     advance();
-    // expression() or equivalent generation
+    expression();
     consume(TOKEN_EOF, "Expect end of expression.");
+    endCompiler();
     return !parser.hadError;
 }
