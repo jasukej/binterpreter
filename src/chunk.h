ee007ffab5329d1e8377c03eb16f8c8abf38faf1
Kezia <roselinakeziarijadi@gmail.com>
2026-03-22 02:18:01 -0700

    debug: add support for new instructions

--- src/chunk.h
+++ src/chunk.h
@@ -6,12 +6,19 @@
 
 typedef enum {
     OP_CONSTANT,
+    OP_NIL,
+    OP_TRUE, // define explicitly instead of loading through OP_CONSTANT
+    OP_FALSE,
+    OP_EQUAL,
+    OP_GREATER,
+    OP_LESS,
     OP_RETURN,
     OP_NEGATE,
     OP_ADD,
     OP_SUBTRACT,
     OP_MULTIPLY,
     OP_DIVIDE,
+    OP_NOT,
 } OpCode;
 
 typedef struct {
